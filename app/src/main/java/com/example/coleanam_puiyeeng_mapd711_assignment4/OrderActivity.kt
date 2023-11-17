17a328ebd4d3114deed34ec8f2d176636a0b16c4
Cole Anam <coleanam2000@gmail.com>
2023-11-17 11:08:16 -0500

    Viewbinding setup

--- app/src/main/java/com/example/coleanam_puiyeeng_mapd711_assignment4/OrderActivity.kt
+++ app/src/main/java/com/example/coleanam_puiyeeng_mapd711_assignment4/OrderActivity.kt
@@ -1,12 +1,18 @@
 package com.example.coleanam_puiyeeng_mapd711_assignment4
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.coleanam_puiyeeng_mapd711_assignment4.databinding.ActivityOrderBinding
 
 class OrderActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityOrderBinding
+    lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_order)
+        binding = ActivityOrderBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
